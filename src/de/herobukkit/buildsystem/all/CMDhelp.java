6f867affd2bc4caba020d45717d4ae836c91ea2b
Ivcx <43705584+GGIvcx@users.noreply.github.com>
2024-09-15 14:54:46 +0200

    Fixed translation

--- src/de/herobukkit/buildsystem/all/CMDhelp.java
+++ src/de/herobukkit/buildsystem/all/CMDhelp.java
@@ -20,37 +20,37 @@ public class CMDhelp implements CommandExecutor{
 					p.sendMessage("§7");
 					p.sendMessage("§7");
 					p.sendMessage("§7");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7You are a §4Administrator§7,");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7Commands:");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/build §8» §7GM1 shortcut");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/jumpto [Player] §8» §7Warp to a Player");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/hide [Player] §8» §7Vanish yourself or others");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/show [Player] §8» §7Unvanish yourself or others");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage("§7");
 				}else if (p.hasPermission("build.team")) {
 					p.sendMessage("§7");
 					p.sendMessage("§7");
 					p.sendMessage("§7");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7You are a §4Administrator§7,");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7Commands:");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/build §8» §7GM1 shortcut");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/jumpto [Player] §8» §7Warp to a Player");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/hide §8» §7Vanish yourself or others");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §9/show §8» §7Unvanish yourself or others");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage("§7");
 				} else {
 					p.sendMessage("§7");
 					p.sendMessage("§7");
 					p.sendMessage("§7");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7On the §9Buildserver §7 only §eBuilders");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7can build. As a §aVisitor");
 					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7you can only spectate.");
-					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHilfecenter§7-x-x-x-x-x-x-");
+					p.sendMessage(ChatColor.translateAlternateColorCodes ('&', Main.getInstance().getConfig().getString("Buildsystem.Prefix")) + " §8| §7-x-x-x-x-x-§aHelp§7-x-x-x-x-x-x-");
 					p.sendMessage("§7");
 				}
 			}
